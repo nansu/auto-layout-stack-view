61dbf7474e91c214b77a3988ea5c312e9c63806e
Nan Su <nsu@salesforce.com>
2019-12-22 16:46:56 -0500

    segues

---
+++ README.md
@@ -0,0 +1,1 @@
+# auto-layout-stack-view
---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,25 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Nan Su on 12/22/19.
+//  Copyright © 2019 Nan Su. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+}
