f2884514a3dfe17d3ce71a49e2bde5014673d0a6
Nan Su <nsu@salesforce.com>
2019-12-22 17:22:23 -0500

    programatic segues

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+        
+    }
     /*
     // MARK: - Navigation
 
